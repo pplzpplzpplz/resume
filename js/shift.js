c42be6fa3b06605b310cd2ad4418aa9cf0b80b20
calebrauxa <moore.caleb@gmail.com>
2024-03-03 18:10:36 -0800

    text shift

--- js/shift.js
+++ js/shift.js
@@ -1,38 +1,46 @@
-let prevMouseX = 0;
-let prevMouseY = 0;
+function preload() {
+  font = loadFont('fonts/syne_variable.ttf');
+}
+
+let pos = 0;
+let ppos = 0;
+let pppos = 0;
+let calebText = 'Caleb Wiseman Taylor Moore';
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
-  frameRate(25);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  frameRate(20);
   blendMode(SUBTRACT);
+  noCursor();
+
+  // FONT
+  textFont(font);
+  textAlign(CENTER, CENTER);
+  textSize(50);
+
+
 }
+
+function mouseWheel(e) {
+  pppos = ppos;
+  ppos = pos;
+  pos += e.delta;
+}
+
 
 function draw() {
   background(255);
   noStroke();
 
-  // GREEN
-  fill(0, 255, 0, 250);
-  circle(convertToWebGLX(prevMouseX), convertToWebGLY(prevMouseY), 50);
+  // let headline = select('h1');  
 
-  // BLUE
-  fill(0, 0, 255, 250);
-  circle(convertToWebGLX(pmouseX), convertToWebGLY(pmouseY), 50);
+  // headline.style('color', 'rgba(255, 0, 0, 0.5)');
+  // headline.style('mix-blend-mode', 'subtract');
 
-  // RED
   fill(255, 0, 0, 250);
-  circle(convertToWebGLX(mouseX), convertToWebGLY(mouseY), 50);
-
-  prevMouseX = pmouseX;
-  prevMouseY = pmouseY;
-}
-
-// Function to convert mouseX coordinate to WebGL coordinate
-function convertToWebGLX(x) {
-  return map(x, 0, width, -width / 2, width / 2);
-}
-
-// Function to convert mouseY coordinate to WebGL coordinate
-function convertToWebGLY(y) {
-  return map(y, 0, height, -height / 2, height / 2);
+  text(calebText, 0, pos);
+  fill(0, 255, 0, 250);
+  text(calebText, 0, ppos);
+  fill(0, 0, 255, 250);
+  text(calebText, 0, pppos);
 }
